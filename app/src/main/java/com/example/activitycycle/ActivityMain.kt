ebaacf8582ae34e1a4c0f69dbd91ffb4c309e33f
JayYadav <jay.yadavinsys@gmail.com>
2025-02-11 12:49:01 +0530

    project created and Activity life Cycle methods are implemented

--- app/src/main/java/com/example/activitycycle/ActivityMain.kt
+++ app/src/main/java/com/example/activitycycle/ActivityMain.kt
@@ -5,28 +5,41 @@ import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
-class ActivityMain: AppCompatActivity (){
+class ActivityMain : AppCompatActivity() {
 
-    @SuppressLint("MissingInflatedId")
+
+   private lateinit var text: TextView
+
+
+    @SuppressLint("MissingInflatedId", "SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main)
 
-        Toast.makeText(this,"OnCreate call", Toast.LENGTH_LONG).show()
-        Log.d("MainActivity","onCreate Method is called")
+        Toast.makeText(this, "OnCreate call", Toast.LENGTH_LONG).show()
+        Log.d("MainActivity", "onCreate Method is called")
 
-        var btn=findViewById<Button>(R.id.btn_1)
-       btn.setOnClickListener { var Intent= Intent(this, MainActivity::class.java)
-       startActivity(Intent)}
+        val btn = findViewById<Button>(R.id.btn_1)
+        btn.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+
+
+        text = findViewById<TextView>(R.id.txt_1)
+        text.setText("This is my First set onCreate")
+
     }
 
     override fun onStart() {
         super.onStart()
         Toast.makeText(this, "onStart call", Toast.LENGTH_LONG).show()
-        Log.d("MainActivity","onStart Method called")
+        Log.d("MainActivity", "onStart Method called")
     }
 
     override fun onRestart() {
@@ -50,7 +63,7 @@ class ActivityMain: AppCompatActivity (){
     override fun onStop() {
         super.onStop()
         Toast.makeText(this, "onStop", Toast.LENGTH_LONG).show()
-        Log.d("MainActivity", "onStop Methid Called")
+        Log.d("MainActivity", "onStop Method Called")
     }
 
     override fun onDestroy() {
